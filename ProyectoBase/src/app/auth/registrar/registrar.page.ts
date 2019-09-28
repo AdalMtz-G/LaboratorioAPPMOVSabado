00172640116d4545549940181170f016f3bbb1cb
AdalMtz-G <54427808+AdalMtz-G@users.noreply.github.com>
2019-09-28 08:58:26 -0500

    Practica login

--- ProyectoBase/src/app/auth/registrar/registrar.page.ts
+++ ProyectoBase/src/app/auth/registrar/registrar.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { IonicRouteStrategy } from '@ionic/angular';
 
 @Component({
   selector: 'app-registrar',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar.page.scss'],
 })
 export class RegistrarPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  registrarForm: FormGroup;
+  submitted =false;
+  constructor(private formBuilder: FormBuilder) { 
+    this.registrarForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      contrasena: ['', [Validators.required, Validators.minLength(6)]],
+      confirmacionContrasena: ['', Validators.required]
+  })
 }
+
+  ngOnInit() {}
+
+  get f() { return this.registrarForm.controls; }
+  
+  registrar() {
+    console.log(this.registrarForm.valid);
+    }
+  }
+/*
+  MustMatch(controlName: string, matchingControlName: string){
+    return true;
+  }
+*/
